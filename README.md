5a2ebd68e825196fd7d55c48fdf410c96c50fb9f
Manish Jethani <manish.jethani@gmail.com>
2015-03-20 20:35:20 +0530

    Version up

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-##### Version 0.2.5
+##### Version 0.3.0
 
 ![Screenshot](red-sands.png)
 
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*  ----------------------------------------------------------------------------
- *  typo v0.2.5
+ *  typo v0.3.0
  *  
  *  Hide secret information in typographical errors
  *  
  *  Author:  Manish Jethani (manish.jethani@gmail.com)
- *  Date:    March 19, 2015
+ *  Date:    March 20, 2015
  *  
  *  See 'typo --help'
  *  
